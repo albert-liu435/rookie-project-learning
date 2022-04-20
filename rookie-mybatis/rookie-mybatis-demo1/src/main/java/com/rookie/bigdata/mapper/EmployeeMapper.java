42f0fc5a218918df9c4a5301aa4aab98f015f604
liuxili01 <liuxili@bestseller.com.cn>
2022-04-20 18:00:19 +0800

    demo1 init

---
+++ rookie-mybatis/rookie-mybatis-demo1/src/main/java/com/rookie/bigdata/mapper/EmployeeMapper.java
@@ -0,0 +1,19 @@
+package com.rookie.bigdata.mapper;
+
+import com.rookie.bigdata.domain.Employee;
+
+/**
+ * @Classname EmployeeMapper
+ * @Description TODO
+ * @Author rookie
+ * @Date 2022/4/19 16:02
+ * @Version 1.0
+ */
+
+public interface EmployeeMapper {
+
+    public Employee getEmpById(Integer id);
+
+}
+
+
